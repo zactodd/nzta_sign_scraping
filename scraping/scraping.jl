2eb4d0cea591ea7a189ed0349661de34584f230e
zactodd <zactodd0@gmail.com>
2020-10-16 14:24:30 +1300

    Update scraping.jl

--- scraping/scraping.jl
+++ scraping/scraping.jl
@@ -4,7 +4,7 @@ scraping:
 - Author: zmt11
 - Date: 2020-10-16
 =#
-using HTTP, Gumbo, Cascadia
+using HTTP, Gumbo, Cascadia, Dates
 
 
 NZTA_URL = "https://www.nzta.govt.nz"
@@ -14,19 +14,23 @@ ALL_SIGNS_URL = "$(SIGNS_SEARCH_URL)/$(SIGN_SEARCH_ALL_QUERY)"
 
 
 get_page(url) = HTTP.get(url).body |> String |> parsehtml
+all_pages(url=SIGNS_SEARCH_URL) = ["$(url)/$(SIGN_SEARCH_ALL_QUERY)&start=$(i)" for i in 0:30:1140]
+download_all_signs(outdir) = map(x -> download_signs(x, outdir), all_pages())
 
-function signs(url=ALL_SIGNS_URL)
+
+function download_signs(url, outdir)
     page = get_page(url)
     rows = eachmatch(sel"div.\[.col.\].typography > table > tbody", page.root)
     table_data = [eachmatch(sel"td", r) for r in rows]
     for r in table_data
-        code = r[1].children[1].text
-        rule = r[2].children[1].text
+        code = replace(r[1].children[1].text, "/" => "_")
         url = "$(NZTA_URL)$(attrs(r[4].children[1])["href"])"
         gif_url = url |> sign_image_nzta_spec
-        HTTP.download(gif_url, "$(code)_$(rule)_image.gif")
+
+        # TODO better names
+        ds = Dates.format(Dates.now(), "yyyy_mm_dd__HH_MM_SS_sss")
+        HTTP.download(gif_url, "$(outdir)/$(ds)_image.gif")
     end
-    return
 end
 
 
@@ -35,6 +39,3 @@ function sign_image_nzta_spec(url)
     gif = eachmatch(sel"div > table:nth-child(5) > tbody > tr:nth-child(5) > td > a", page.root)[1]
     return "$(NZTA_URL)$(attrs(gif)["href"])"
 end
-
-
-signs()

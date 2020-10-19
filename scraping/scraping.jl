5343a8555b22a5fbe29ce499e07eadbde7365b2f
zactodd <zactodd0@gmail.com>
2020-10-19 16:34:46 +1300

    tidy up

--- scraping/scraping.jl
+++ scraping/scraping.jl
@@ -9,25 +9,28 @@ ALL_SIGNS_URL = "$SIGNS_SEARCH_URL/$SIGN_SEARCH_ALL_QUERY"
 get_page(url) = HTTP.get(url).body |> String |> parsehtml
 all_pages(url=SIGNS_SEARCH_URL) = ["$url/$SIGN_SEARCH_ALL_QUERY&start=$(i)" for i in 0:30:1155]
 download_all_signs(outdir) = map(x -> download_signs(x, outdir), all_pages())
+spec_url(row) = "$NZTA_URL$(attrs(row[4].children[1])["href"])"
 
 
 function download_signs(url, outdir)
     page = get_page(url)
     rows = eachmatch(sel"div.\[.col.\].typography > table > tbody", page.root)
-    table_data = [eachmatch(sel"td", r) for r in rows]
-    for r in table_data
-        url = "$NZTA_URL$(attrs(r[4].children[1])["href"])"
-        image_url = url |> sign_image_nzta_spec
-        name = replace(split(image_url, "/")[end], ".gif" => ".png")
-        HTTP.download(image_url, "$outdir/$name")
+    image_urls = [eachmatch(sel"td", r) |> spec_url |> image_from_spec for r in rows]
+    for url in skipmissing(image_urls)
+        name = replace(split(url, "/")[end], ".gif" => ".png")
+        HTTP.download(url, "$outdir/$name")
     end
 end
 
 
-function sign_image_nzta_spec(url)
+function image_from_spec(url)
     page = get_page(url)
-    gif = eachmatch(sel"div > table:nth-child(5) > tbody > tr:nth-child(5) > td > a", page.root)[1]
-    return "$NZTA_URL$(attrs(gif)["href"])"
+    matches = eachmatch(sel"div > table:nth-child(5) > tbody > tr:nth-child(5) > td > a", page.root)
+    if length(matches) == 0
+        return missing
+    else
+        return "$NZTA_URL$(attrs(matches[1])["href"])"
+    end
 end
 
 
